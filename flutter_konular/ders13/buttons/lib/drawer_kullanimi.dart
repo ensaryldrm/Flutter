937135ab80f6cbcdfb689096d034c50d6e62b63b
ensaryldrm <Ensar_Yldrm@outlook.com>
2025-06-27 16:11:04 +0300

    yeni konular eklendi

--- flutter_konular/ders13/buttons/lib/drawer_kullanimi.dart
+++ flutter_konular/ders13/buttons/lib/drawer_kullanimi.dart
@@ -1,3 +1,6 @@
+import 'package:buttons/sayfa1.dart';
+import 'package:buttons/sayfa2.dart';
+import 'package:buttons/sayfa3.dart';
 import 'package:flutter/material.dart';
 
 class DrawerKullanimi extends StatefulWidget {
@@ -8,13 +11,87 @@ class DrawerKullanimi extends StatefulWidget {
 }
 
 class _DrawerKullanimiState extends State<DrawerKullanimi> {
+  List<Widget> sayfaList = [Sayfa1(), Sayfa2(), Sayfa3()];
+  int secilenIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.brown.shade200,
       appBar: AppBar(
-        backgroundColor: Colors.blueGrey.shade400,
+        backgroundColor: Colors.blueGrey.shade700,
         foregroundColor: Colors.white,
         title: Text("Drawer Kullanımı"),
+      ),
+      body: sayfaList[secilenIndex],
+      drawer: Drawer(
+        width: 230,
+        backgroundColor: Colors.blueGrey.shade200,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            // ANA BAŞLIK
+            SizedBox(
+              height: 150,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 4),
+                child: DrawerHeader(
+                  margin: EdgeInsets.only(
+                    top: 10,
+                    left: 20,
+                    right: 20,
+                    bottom: 10,
+                  ),
+
+                  decoration: BoxDecoration(
+                    color: Colors.blueGrey.shade700,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(10),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Text(
+                      "Butonlar",
+                      style: TextStyle(color: Colors.white, fontSize: 30),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Ana Sayfa"),
+              onTap: () {
+                setState(() {
+                  secilenIndex = 0;
+                  Navigator.pop(context);
+                });
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("Ayarlar"),
+              onTap: () {
+                setState(() {
+                  secilenIndex = 1;
+                  Navigator.pop(context);
+                });
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.place_rounded),
+              title: Text("Konum"),
+              onTap: () {
+                setState(() {
+                  secilenIndex = 2;
+                  Navigator.pop(context);
+                });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
