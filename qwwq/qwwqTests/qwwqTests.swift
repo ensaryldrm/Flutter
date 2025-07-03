68e770295f5eb9e6a972805c882a94f0b6592c64
EnsarYldrm <Ensar_Yldrm@outlook.com>
2025-07-03 09:14:03 +0300

    not kaldırıldı

---
+++ qwwq/qwwqTests/qwwqTests.swift
@@ -0,0 +1,16 @@
+//
+//  qwwqTests.swift
+//  qwwqTests
+//
+//  Created by 3-09 on 1.07.2025.
+//
+
+import Testing
+
+struct qwwqTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
